df39c2e43fbe16282e210b19ae4dba6937515c13
A-Godgi <88877699+A-Godgi@users.noreply.github.com>
2024-09-20 13:48:48 +0300

    add App root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './assets/styles/main.scss'
+import Layout from './ui/Layout'
+import HomePage from './pages/HomePage'
+import useFetch from './hooks/useFetch'
+import { fetchAllExchangeRates, getCurrencies } from './services/api'
+import { Currency, ExchangeRateRes } from './types'
+import { getCurrenciesList } from './services/getCurrenciesList'
+import { getSelectedCurrenciesFromStore } from './services/selectedCurrencies'
+import ExchangeRate from './components/ExchangeRate'
+import EditPanel from './components/EditPanel'
+import { useHandleRequests } from './hooks/useHandleRequests'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const {
+		selectedCurrencies,
+		setSelectedCurrencies,
+		isEditable,
+		setIsEditable,
+		isLoadingRates,
+		dataRates,
+		currenciesList,
+	} = useHandleRequests()
+
+	const handleEdit = () => {
+		setIsEditable(true)
+	}
+
+	const handleSave = (interimList: Currency[]) => {
+		setSelectedCurrencies(interimList)
+		localStorage.setItem('selectedCurrencies', JSON.stringify(interimList))
+		setIsEditable(false)
+	}
+
+	return (
+		<Layout
+			header={
+				isEditable ? (
+					<EditPanel
+						selectedCurrencies={selectedCurrencies}
+						handleSave={handleSave}
+						currenciesList={currenciesList}
+					/>
+				) : isLoadingRates ? (
+					'Loading...'
+				) : (
+					<ExchangeRate
+						handleEdit={handleEdit}
+						rates={dataRates}
+						selectedCurrencies={selectedCurrencies}
+					/>
+				)
+			}
+		>
+			<HomePage />
+		</Layout>
+	)
 }
 
-export default App;
+export default App
